09ea133c659420436a827a5acfaeeaedc3d7a96d
ppq-dev <yxy1223ppq@gmail.com>
2025-09-09 16:11:19 +0800

    Add placeholder for example_instructions.py

---
+++ example_instructions.py
@@ -0,0 +1,3 @@
+# Placeholder for example_instructions.py from meta-llama/codellama
+# This file will contain example instructions for the fine-tuning framework
+# The actual content should be copied from: https://github.com/meta-llama/codellama/blob/main/example_instructions.py
